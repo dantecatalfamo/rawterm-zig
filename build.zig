0b0d663342cf84349b19b91247f647b146c0d8a8
dantecatalfamo <dante.catalfamo@gmail.com>
2021-11-18 17:39:39 -0500

    Fix name in build

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("selector-zig", "src/main.zig");
+    const lib = b.addStaticLibrary("rawterm", "src/main.zig");
     lib.setBuildMode(mode);
     lib.install();
 
