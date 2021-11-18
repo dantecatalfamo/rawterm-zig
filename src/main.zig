89f4caf9b66cc5f9361cc5f0d56f88510481d741
dantecatalfamo <dante.catalfamo@gmail.com>
2021-11-18 17:35:27 -0500

    Begin putting terminal into raw mode

---
+++ src/main.zig
@@ -0,0 +1,76 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const os = std.os;
+const system = os.system;
+const testing = std.testing;
+
+pub fn raw() !void {
+    var termios = try os.tcgetattr(os.STDIN_FILENO);
+    const vmin = if (@hasField(system, "V")) system.V.MIN else std.c.VMIN;
+    const vtime = if (@hasField(system, "V")) system.V.TIME else std.c.VTIME;
+
+    termios.iflag &= ~(@as(@TypeOf(termios.iflag), system.IGNBRK |
+        system.BRKINT |
+        system.IGNPAR |
+        system.PARMRK |
+        system.INPCK |
+        system.ISTRIP |
+        system.INLCR |
+        system.IGNCR |
+        system.ICRNL |
+        system.IXON |
+        system.IXOFF |
+        system.IXANY |
+        system.IMAXBEL));
+    termios.oflag &= ~(@as(@TypeOf(termios.oflag), system.OPOST));
+    termios.lflag &= ~(@as(@TypeOf(termios.lflag), system.ECHO |
+        system.ECHOE |
+        system.ECHOK |
+        system.ECHONL |
+        system.ICANON |
+        system.ISIG |
+        system.IEXTEN
+    // | system.XCASE
+    ));
+    termios.cflag &= ~(@as(@TypeOf(termios.cflag), system.CSIZE | system.PARENB));
+    termios.cflag |= (system.CS8);
+    termios.cc[vmin] = 1;
+    termios.cc[vtime] = 0;
+
+    try os.tcsetattr(os.STDIN_FILENO, system.TCSA.FLUSH, termios);
+}
+
+pub fn rawOff() !void {
+    var termios = try os.tcgetattr(os.STDIN_FILENO);
+
+    termios.iflag |= (system.BRKINT | system.ISTRIP | system.ICRNL | system.IXON);
+    termios.oflag |= (system.OPOST);
+    termios.lflag |= (system.ECHO | system.ECHOE | system.ECHOK | system.ECHONL | system.ICANON | system.ISIG | system.IEXTEN);
+
+    try os.tcsetattr(os.STDIN_FILENO, os.TCSA.FLUSH, termios);
+}
+
+pub fn main() !void {
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    try raw();
+    while (true) {
+        const byte = try stdin.readByte();
+        switch (byte) {
+            'c' => {
+                try stdout.print("raw off\n", .{});
+                try rawOff();
+            },
+            'e' => {
+                try stdout.print("raw on\n", .{});
+                try raw();
+            },
+            else => {
+                try stdout.print("exiting\n", .{});
+                try rawOff();
+                std.os.exit(0);
+            },
+        }
+    }
+}
